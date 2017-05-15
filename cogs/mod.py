e81f06b529f8e28c317efae68ef4d7ef70257ea5
Colton Wiens <XNBlank@users.noreply.github.com>
2017-05-15 03:31:31 -0400

    removed semicolons

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,18 +1,18 @@
-import discord;
-from discord.ext import commands;
-from .init import checks;
-import json;
-import os.path;
-import asyncio;
-import sys;
+import discord
+from discord.ext import commands
+from .init import checks
+import json
+import os.path
+import asyncio
+import sys
 
 class Mod:
     """ Moderative Commands """
 
     def __init__(self, bot, config, thread=None):
-        self.bot = bot;
-        self.config = config;
-        self.thr = thread;
+        self.bot = bot
+        self.config = config
+        self.thr = thread
 
     @commands.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(kick_members=True)
@@ -22,36 +22,36 @@ class Mod:
         To use this command you must have Kick Members permission or have the Admin role.
         """
 
-        channels = self.bot.get_all_channels();
-        moderator = ctx.message.author;
-        mod_role = None;
-        roles = ctx.message.server.roles;
+        channels = self.bot.get_all_channels()
+        moderator = ctx.message.author
+        mod_role = None
+        roles = ctx.message.server.roles
 
-        print(channels);
+        print(channels)
 
         if(reason == None):
 
             for idx, i in enumerate(roles):
-                role = i.name;
+                role = i.name
                 if(role == "Moderator"):
-                    mod_role = i;
+                    mod_role = i
 
-            reason = 'Reason not given. {0} feel free to edit this message to put a reason.'.format(mod_role.mention);
+            reason = 'Reason not given. {0} feel free to edit this message to put a reason.'.format(mod_role.mention)
 
-        log_chan = self.config['log_channel'];
-        chan_id = 0;
+        log_chan = self.config['log_channel']
+        chan_id = 0
 
         for i in channels:
             if log_chan in i.name:
-                chan_id = i;
-                print('Channel Name is ' + chan_id.name);
+                chan_id = i
+                print('Channel Name is ' + chan_id.name)
 
         try:
-            await self.bot.kick(user);
+            await self.bot.kick(user)
         except discord.Forbidden:
-            await self.bot.say('The bot does not have permissions to kick members.');
+            await self.bot.say('The bot does not have permissions to kick members.')
         else:
-            await self.bot.send_message(chan_id, 'User **{0} : {1}** was kicked for reason :\n***{2}***\nKicked by : **{3}**'.format(user.name, user.id, reason, moderator.name));
+            await self.bot.send_message(chan_id, 'User **{0} : {1}** was kicked for reason :\n***{2}***\nKicked by : **{3}**'.format(user.name, user.id, reason, moderator.name))
 
     @commands.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(ban_members=True)
@@ -61,36 +61,36 @@ class Mod:
         To use this command you must have Ban Members permission or have the Admin role.
         """
 
-        channels = self.bot.get_all_channels();
-        moderator = ctx.message.author;
-        mod_role = None;
-        roles = ctx.message.server.roles;
+        channels = self.bot.get_all_channels()
+        moderator = ctx.message.author
+        mod_role = None
+        roles = ctx.message.server.roles
 
-        print(channels);
+        print(channels)
 
         if(reason == None):
 
             for idx, i in enumerate(roles):
-                role = i.name;
+                role = i.name
                 if(role == "Moderator"):
-                    mod_role = i;
+                    mod_role = i
 
-            reason = 'Reason not given. {0} feel free to edit this message to put a reason.'.format(mod_role.mention);
+            reason = 'Reason not given. {0} feel free to edit this message to put a reason.'.format(mod_role.mention)
 
-        log_chan = self.config['log_channel'];
-        chan_id = 0;
+        log_chan = self.config['log_channel']
+        chan_id = 0
 
         for i in channels:
             if log_chan in i.name:
-                chan_id = i;
-                print('Channel Name is ' + chan_id.name);
+                chan_id = i
+                print('Channel Name is ' + chan_id.name)
 
         try:
-            await self.bot.ban(user);
+            await self.bot.ban(user)
         except discord.Forbidden:
-            await self.bot.say('The bot does not have permissions to ban members.');
+            await self.bot.say('The bot does not have permissions to ban members.')
         else:
-            await self.bot.send_message(chan_id, 'User **{0} : {1}** was banned for reason :\n***{2}***\nBanned by : **{3}**'.format(user.name, user.id, reason, moderator.name));
+            await self.bot.send_message(chan_id, 'User **{0} : {1}** was banned for reason :\n***{2}***\nBanned by : **{3}**'.format(user.name, user.id, reason, moderator.name))
 
 
     @commands.command(pass_context=True, no_pm=True)
@@ -101,20 +101,20 @@ class Mod:
         To use the command you must have Manage Messages permissions or have the Admin role.
         """
 
-        channel = ctx.message.channel;
+        channel = ctx.message.channel
 
-        calls = 0;
+        calls = 0
         async for msg in self.bot.logs_from(channel, limit=amount, before=ctx.message):
             if calls and calls % 5 == 0:
-                await asyncio.sleep(1.5);
+                await asyncio.sleep(1.5)
 
             if msg.author == self.bot.user:
-                await self.bot.delete_message(msg);
-                calls += 1;
+                await self.bot.delete_message(msg)
+                calls += 1
         if calls == 1:
-            await self.bot.say('`Pruned {0} message.`'.format(calls));
+            await self.bot.say('`Pruned {0} message.`'.format(calls))
         else:
-            await self.bot.say('`Pruned {0} messages.`'.format(calls));
+            await self.bot.say('`Pruned {0} messages.`'.format(calls))
 
     
     @commands.command(pass_context=True, no_pm=True)
@@ -125,93 +125,93 @@ class Mod:
         To use this command you must have Ban Members permission or have the Admin role.
         """
 
-        channels = self.bot.get_all_channels();
-        moderator = ctx.message.author;
-        mod_role = None;
-        roles = ctx.message.server.roles;
+        channels = self.bot.get_all_channels()
+        moderator = ctx.message.author
+        mod_role = None
+        roles = ctx.message.server.roles
 
-        warnCount = 0;
+        warnCount = 0
 
         if(reason == None):
 
             for idx, i in enumerate(roles):
-                role = i.name;
+                role = i.name
                 if(role == "Moderator"):
-                    mod_role = i;
+                    mod_role = i
 
-            reason = 'Reason not given. {0} feel free to edit this message to put a reason.'.format(mod_role.mention);
+            reason = 'Reason not given. {0} feel free to edit this message to put a reason.'.format(mod_role.mention)
 
-        dataFile = 'data/warns/warns.dat';
-        data = {};
+        dataFile = 'data/warns/warns.dat'
+        data = {}
 
-        print(os.path.isfile(dataFile));
+        print(os.path.isfile(dataFile))
 
         if(os.path.isfile(dataFile)):
             with open(dataFile, 'r', encoding='utf8') as f:
-                data = json.load(f);
-                print(data);
-                print('closed');
+                data = json.load(f)
+                print(data)
+                print('closed')
             
             if(user.id not in data):
-                data['{0}'.format(user.id)] = '{0}'.format(points);
-                warnCount = points;
+                data['{0}'.format(user.id)] = '{0}'.format(points)
+                warnCount = points
             else:
-                warnCount = data["{0}".format(user.id)];
-                data["{0}".format(user.id)] = int(warnCount) + points;
-                warnCount = data["{0}".format(user.id)];
+                warnCount = data["{0}".format(user.id)]
+                data["{0}".format(user.id)] = int(warnCount) + points
+                warnCount = data["{0}".format(user.id)]
             
             with open(dataFile, 'w', encoding='utf8') as f:
-                f.write(json.dumps(data));
-                print('written');
+                f.write(json.dumps(data))
+                print('written')
 
         else:
             data = {
                 "{0}".format(user.id) : "{0}".format(points)
-            };
+            }
 
-            data["{0}".format(user.id)] = warnCount;
-            warnCount = points;
+            data["{0}".format(user.id)] = warnCount
+            warnCount = points
             
             async with open(dataFile, 'w', encoding='utf8') as f:
-                f.write(json.dumps(data));
-                print('written');
-                f.close();
+                f.write(json.dumps(data))
+                print('written')
+                f.close()
 
-            print(data);
+            print(data)
 
-        # print(channels);
+        # print(channels)
 
-        log_chan = self.config['log_channel'];
-        chan_id = 0;
+        log_chan = self.config['log_channel']
+        chan_id = 0
 
         for i in channels:
             if log_chan in i.name:
-                chan_id = i;
-                # print('Channel Name is ' + chan_id.name);
+                chan_id = i
+                # print('Channel Name is ' + chan_id.name)
         
-        print(warnCount);
+        print(warnCount)
 
         try:
             if(int(warnCount) >= 1000):
-                print("Banned!");
-                await self.bot.ban(user);
-                await self.bot.send_message(chan_id, 'User **{0} : {1}** was banned for reason :\n***{2}***\nBanned by : **{3}**'.format(user.name, user.id, "Exceeding warning points. (1000)", moderator.name));
+                print("Banned!")
+                await self.bot.ban(user)
+                await self.bot.send_message(chan_id, 'User **{0} : {1}** was banned for reason :\n***{2}***\nBanned by : **{3}**'.format(user.name, user.id, "Exceeding warning points. (1000)", moderator.name))
             elif(int(warnCount) > 800 and int(warnCount) < 1000):
-                await self.bot.kick(user);
-                await self.bot.send_message(chan_id, 'User **{0} : {1}** was kicked for reason :\n***{2}***\nKicked by : **{3}**'.format(user.name, user.id, "Exceeding warning points. (600)", moderator.name));
+                await self.bot.kick(user)
+                await self.bot.send_message(chan_id, 'User **{0} : {1}** was kicked for reason :\n***{2}***\nKicked by : **{3}**'.format(user.name, user.id, "Exceeding warning points. (600)", moderator.name))
             else:
-                print("warned");
+                print("warned")
         except discord.Forbidden:
-            await self.bot.say('The bot does not have permissions to ban members.');
+            await self.bot.say('The bot does not have permissions to ban members.')
         else:
-            print("send messages");
+            print("send messages")
             if(int(warnCount) >= 1000):
-                await self.bot.send_message(chan_id, 'User **{0} : {1}** was banned for reason :\n***Exceeding warning points.***\nBanned by : **{2}**'.format(user.name, user.id, moderator.name));
+                await self.bot.send_message(chan_id, 'User **{0} : {1}** was banned for reason :\n***Exceeding warning points.***\nBanned by : **{2}**'.format(user.name, user.id, moderator.name))
             elif(int(warnCount) > 800 and int(warnCount) < 1000):
-                await self.bot.kick(user);
-                await self.bot.send_message(chan_id, 'User **{0} : {1}** was kicked for reason :\n***{2}***\nKicked by : **{3}**'.format(user.name, user.id, "Exceeding warning points. (600)", moderator.name));
+                await self.bot.kick(user)
+                await self.bot.send_message(chan_id, 'User **{0} : {1}** was kicked for reason :\n***{2}***\nKicked by : **{3}**'.format(user.name, user.id, "Exceeding warning points. (600)", moderator.name))
             else:
-                await self.bot.send_message(chan_id, 'User **{0} : {1}** was warned for reason :\n***{2}***\nWarned by : **{3}**\nWarning Points : **{4}**'.format(user.name, user.id, reason, moderator.name, warnCount));
+                await self.bot.send_message(chan_id, 'User **{0} : {1}** was warned for reason :\n***{2}***\nWarned by : **{3}**\nWarning Points : **{4}**'.format(user.name, user.id, reason, moderator.name, warnCount))
 
 
     @commands.command(pass_context=True, no_pm=True)
@@ -222,37 +222,37 @@ class Mod:
         To use this command you must have Ban Members permission or have the Admin role.
         """
 
-        channels = self.bot.get_all_channels();
-        moderator = ctx.message.author;
-        warnCount = 0;
-        dataFile = 'data/warns/warns.dat';
-        data = {};
+        channels = self.bot.get_all_channels()
+        moderator = ctx.message.author
+        warnCount = 0
+        dataFile = 'data/warns/warns.dat'
+        data = {}
 
         with open(dataFile, 'r', encoding='utf8') as f:
-            data = json.load(f);
-            print(data);
-            print('closed');
-            f.close();
+            data = json.load(f)
+            print(data)
+            print('closed')
+            f.close()
 
-        log_chan = self.config['log_channel'];
-        chan_id = 0;
+        log_chan = self.config['log_channel']
+        chan_id = 0
 
         for i in channels:
             if log_chan in i.name:
-                chan_id = i;
+                chan_id = i
 
         if(user.id not in data or int(data["{0}".format(user.id)] == 0)):
-            await self.bot.send_message(ctx.message.channel, 'That user doesn\'t have any warning points.');
+            await self.bot.send_message(ctx.message.channel, 'That user doesn\'t have any warning points.')
         else:
-            warnCount = data["{0}".format(user.id)];
-            data["{0}".format(user.id)] = int(warnCount) - points;
-            warnCount = data["{0}".format(user.id)];
+            warnCount = data["{0}".format(user.id)]
+            data["{0}".format(user.id)] = int(warnCount) - points
+            warnCount = data["{0}".format(user.id)]
 
             with open(dataFile, 'w', encoding='utf8') as f:
-                f.write(json.dumps(data));
-                print('written');
-                f.close();
+                f.write(json.dumps(data))
+                print('written')
+                f.close()
 
-            await self.bot.send_message(chan_id, 'User **{0} : {1}** was lifted {2} warning points.\nAction by : **{3}**\nRemaining Warning Points : **{4}**'.format(user.name, user.id, points, moderator.name, warnCount));
+            await self.bot.send_message(chan_id, 'User **{0} : {1}** was lifted {2} warning points.\nAction by : **{3}**\nRemaining Warning Points : **{4}**'.format(user.name, user.id, points, moderator.name, warnCount))
 
 
